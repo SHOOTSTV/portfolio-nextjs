b7ee36c35d60d191d3c1b04678442176095593d1
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2025-01-27 20:27:55 +0100

    feat: Add Projects section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,16 +8,17 @@ import Image from "next/image";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="relative z-0 overflow-x-clip py-32 md:py-48 lg:py-60">
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{ backgroundImage: `url(${grainImage.src})` }}
         ></div>
-        <div className="size-[620px] hero-ring"></div>
-        <div className="size-[820px] hero-ring"></div>
-        <div className="size-[1020px] hero-ring"></div>
-        <div className="size-[1220px] hero-ring"></div>
+        {/* Green Rings */}
+        <div className="hero-ring size-[620px]"></div>
+        <div className="hero-ring size-[820px]"></div>
+        <div className="hero-ring size-[1020px]"></div>
+        <div className="hero-ring size-[1220px]"></div>
         {/* Orbits Stars */}
         <HeroOrbit size={800} rotation={-72}>
           <StarIcon className="size-28 text-emerald-300" />
@@ -57,15 +58,15 @@ export const HeroSection = () => {
             className="size-[100px]"
             alt="Person peeking from behind a laptop"
           />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="size-2.5 bg-green-500 rounded-full"></div>
+          <div className="inline-flex items-center gap-4 rounded-lg border border-gray-800 bg-gray-950 px-4 py-1.5">
+            <div className="size-2.5 rounded-full bg-green-500"></div>
             <div className="text-sm font-medium">
               Available for new projects
             </div>
           </div>
         </div>
-        <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+        <div className="mx-auto max-w-lg">
+          <h1 className="mt-8 text-center font-serif text-3xl tracking-wide md:text-5xl">
             Building Exceptional User Experiences
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
@@ -73,12 +74,12 @@ export const HeroSection = () => {
             exceptional user experiences.
           </p>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+        <div className="mt-8 flex flex-col items-center justify-center gap-4 md:flex-row">
+          <button className="inline-flex h-12 items-center gap-2 rounded-xl border border-white/15 px-6">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
           </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <button className="inline-flex h-12 items-center gap-2 rounded-xl border border-white bg-white px-6 text-gray-900">
             <span>👋</span>
             <span className="font-semibold">Let's Connect</span>
           </button>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,120 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+// Import icons and required images
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import portfolioAxel from "@/assets/images/portfolio-axel-react.png";
+import trelloClone from "@/assets/images/trello-clone.png";
+import Image from "next/image";
 
+// Portfolio projects data
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "Trello Clone",
+    year: "2024",
+    title: "Web App Collaborative",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Authentication with Workspaces" },
+      { title: "Unsplash API & Activity Logging System" },
+      { title: "Stripe Subscription & Unlimited Boards" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://trello-clone-weld-tau.vercel.app/",
+    image: trelloClone,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "Personal Portfolio",
+    year: "2025",
+    title: "Animated React Website",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: "React & Next.js Foundations" },
+      { title: "TailwindCSS Mastery" },
+      { title: "Framer Motion & CSS Animations" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
-  },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    link: "",
+    image: portfolioAxel,
   },
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        {/* Section header */}
+        <div className="flex justify-center">
+          <p className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text font-semibold uppercase tracking-widest text-transparent">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="mt-6 text-center font-serif text-3xl md:text-5xl">
+          Featured Projects
+        </h2>
+        <p className="mx-auto mt-4 max-w-md text-center text-white/60 md:text-lg lg:text-xl">
+          See how I transformed concepts into engaging digital experiences
+        </p>
+
+        {/* Projects list */}
+        <div className="mt-10 flex flex-col gap-20 md:mt-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="relative z-0 overflow-hidden rounded-3xl bg-gray-800 px-8 pt-8 after:pointer-events-none after:absolute after:inset-0 after:z-10 after:rounded-3xl after:outline after:outline-2 after:-outline-offset-2 after:outline-white/20 after:content-[''] md:px-10 md:pt-12 lg:px-20 lg:pt-16"
+            >
+              {/* Background grain effect */}
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{ backgroundImage: `url(${grainImage.src})` }}
+              ></div>
+              {/* Start of grid layout */}
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                {/* Left grid */}
+                <div className="lg:pb-16">
+                  {/* Project header */}
+                  <div className="inline-flex gap-2 bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-sm font-bold uppercase tracking-widest text-transparent">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="mt-2 font-serif text-2xl md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+
+                  {/* Divider */}
+                  <hr className="mt-4 border-t-2 border-white/5 md:mt-5" />
+
+                  {/* Results list */}
+                  <ul className="mt-4 flex flex-col gap-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li
+                        key={result.title}
+                        className="flex gap-2 text-sm text-white/50 md:text-base"
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+
+                  {/* Site link button */}
+                  <a href={project.link}>
+                    <button className="mt-8 inline-flex h-12 w-full items-center justify-center gap-2 rounded-xl bg-white px-6 font-semibold text-gray-950 md:w-auto">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                {/* Right grid */}
+                <div className="relative">
+                  {/* Project image */}
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="-mb-4 mt-8 rounded-t-3xl border-2 border-white/20 md:-mb-0 lg:absolute lg:mt-0 lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
