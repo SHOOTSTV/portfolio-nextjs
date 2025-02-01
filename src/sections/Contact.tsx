234f64c2b1ae5938e258e37a37c2cdf0a8434b6a
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2025-02-01 17:16:33 +0100

    feat: Add Contact section with call-to-action design

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,34 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="relative z-0 overflow-hidden rounded-3xl bg-gradient-to-r from-emerald-300 to-sky-400 px-10 py-8 text-center text-gray-900 md:text-left">
+          <div
+            className="absolute inset-0 -z-10 opacity-5"
+            style={{ backgroundImage: `url(${grainImage.src})` }}
+          ></div>
+          <div className="flex flex-col items-center gap-8 md:flex-row md:gap-16">
+            <div>
+              <h2 className=" font-serif text-2xl md:text-3xl">
+                Let's create something amazing together
+              </h2>
+              <p className="mt-2 text-sm md:text-base">
+                Ready to bring your ideas to life? Let's discuss your project
+                and how I can help you achieve your goals.
+              </p>
+            </div>
+            <div>
+              <button className="inline-flex h-12 w-max items-center gap-2 rounded-xl border border-gray-900 bg-gray-900 px-6 text-white">
+                <span className="font-medium">Contact me</span>
+                <ArrowUpRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
